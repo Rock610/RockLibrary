a14d05ee81537060b044e648a251aec3eab82ff6
wangyiran <wangyiran@wdwd.com>
2015-10-20 11:42:57 +0800

    增加了通用的titleBar组件

---
+++ app/src/main/java/com/rock/android/rocklibrary/ui/BaseActivity.java
@@ -0,0 +1,11 @@
+package com.rock.android.rocklibrary.ui;
+
+import android.app.Activity;
+
+/**
+ * Created by rock on 15/10/20.
+ */
+public class BaseActivity extends Activity{
+
+
+}
--- app/src/main/java/com/rock/android/rocklibrary/ui/MainActivity.java
+++ app/src/main/java/com/rock/android/rocklibrary/ui/MainActivity.java
@@ -1,35 +1,34 @@
 package com.rock.android.rocklibrary.ui;
 
-import android.app.Activity;
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.view.View;
 
 import com.rock.android.rocklibrary.NetWork.BaseNetWorkHelper;
 import com.rock.android.rocklibrary.NetWork.Controller;
 import com.rock.android.rocklibrary.R;
 import com.rock.android.rocklibrary.Utils.Image.CameraCore;
 
-public class MainActivity extends Activity implements BaseNetWorkHelper.IhttpResponseHandler,CameraCore.CameraResult{
+public class MainActivity extends TitleBarBaseActivity implements BaseNetWorkHelper.IhttpResponseHandler,CameraCore.CameraResult{
 
     private String url;
     private CameraCore mCameraCore;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        mCameraCore = new CameraCore(this,this);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-//                Uri uri = Uri.parse("test.png");
-//                mCameraCore.getPhotoFromAlbum(uri);
-                mCameraCore.getAlbumData(MainActivity.this);
-            }
-        });
+        setContentViewRes(R.layout.content_main);
+        setTitleText("我是Title");
+        mTitleBar.getTitleText().setTextColor(Color.WHITE);
+//        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+//        mCameraCore = new CameraCore(this,this);
+//        fab.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+////                Uri uri = Uri.parse("test.png");
+////                mCameraCore.getPhotoFromAlbum(uri);
+//                mCameraCore.getAlbumData(MainActivity.this);
+//            }
+//        });
         url = "http://www.weather.com.cn/adat/sk/101010100.html";
         Controller.requestTest(url,this);
     }
